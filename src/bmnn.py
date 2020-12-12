198dc50668130c74ad691da7962d984225ba209c
dbisk <16860792+dbisk@users.noreply.github.com>
2020-12-12 00:38:34 -0600

    improved block-matching, other small changes that should not affect performance

--- src/bmnn.py
+++ src/bmnn.py
@@ -1,5 +1,7 @@
 # bmnn.py
 
+import heapq
+
 import numpy as np
 import torch
 import torch.nn as nn
@@ -8,14 +10,20 @@ import torch.optim as optim
 # some parameters
 PATCH_SIZE = 8
 SEARCH_SIZE = 39
-THRESHOLD = 350,
-MAX_BLOCKS = 5
+THRESHOLD = 1300,
+MAX_BLOCKS = 12
 
 class Block():
   def __init__(self, patch, x, y):
     self.x = x
     self.y = y
     self.data = patch
+  
+  def __lt__(self, other):
+    return self.x < other.x
+  
+  def __gt__(self, other):
+    return self.x > other.x
 
 def blocks_to_array(blocks):
   out = np.zeros((len(blocks), PATCH_SIZE, PATCH_SIZE))
@@ -49,9 +57,9 @@ def bmnn(img, model, stride=1):
     out = model(torch.unsqueeze(full_img, 0))
   return torch.squeeze(out).numpy()
 
-def blockmatch(img, coords, search_size=SEARCH_SIZE, patch_size=PATCH_SIZE, threshold=THRESHOLD, stride=1, N=MAX_BLOCKS):
+def blockmatch(img, coords, search_size=SEARCH_SIZE, patch_size=PATCH_SIZE, threshold=THRESHOLD, stride=1, N=MAX_BLOCKS, stats=None):
   blocks = []
-  patch_x = coords[0] # TODO: double check this, might be backwards
+  patch_x = coords[0]
   patch_y = coords[1]
   start_x = 0 if patch_x - search_size/2 < 0 else int(patch_x - search_size/2)
   start_y = 0 if patch_y - search_size/2 < 0 else int(patch_y - search_size/2)
@@ -59,26 +67,28 @@ def blockmatch(img, coords, search_size=SEARCH_SIZE, patch_size=PATCH_SIZE, thre
   end_y = img.shape[1] if patch_y + search_size/2 > img.shape[1] else int(patch_y + search_size/2)
 
   base = img[patch_x:patch_x + patch_size, patch_y:patch_y + patch_size]
+  # TODO: can check for a faster BM algorithm than brute force
   for x in range(start_x, end_x - patch_size, stride):
     for y in range(start_y, end_y - patch_size, stride):
       # grab each patch and compare to base patch
       patch = img[x:x + patch_size, y:y + patch_size]
-      diff = np.linalg.norm(base - patch)
-      if (diff < threshold):
-        blocks.append(Block(patch, x, y))
-
-  # catch the very odd case in which there are no matches and we accidentally skipped over the 
-  # original patch itself
-  if (len(blocks) == 0):
-    blocks.append(Block(base, patch_x, patch_y))
+      mse = np.mean(np.square(base - patch))
+      if (mse < threshold):
+        heapq.heappush(blocks, (mse, Block(patch, x, y)))
+        # debug
+        # TODO: change the way this is done, currently needs stats to be a list due to
+        # python not having a way to pass by reference
+        if stats is not None:
+          stats[0] += 1
 
   # add copies of the original patch until we are at N blocks (TODO: revisit this)
   while (len(blocks) < N):
-    blocks.append(Block(base, patch_x, patch_y))
+    heapq.heappush(blocks, (0, Block(base, patch_x, patch_y)))
   
-  # remove random entries until we are at N blocks (TODO: revisit this)
-  while (len(blocks) > N):
-    blocks.pop(np.random.randint(0, len(blocks)))
+  # take the top N blocks
+  result = []
+  for i in range(N):
+    result.append(heapq.heappop(blocks)[1])
 
-  return blocks
+  return result
 
